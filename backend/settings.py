0887d68146c66917787902b89d8daf14d00bab64
Sharif <fathifarhan21@hotmail.com>
2022-04-02 00:02:10 +0100

    --

--- backend/settings.py
+++ backend/settings.py
@@ -138,8 +138,13 @@ REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny'
-    ]
+        'rest_framework.permissions.AllowAny',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+
+
 }
 
 
