90dc2f84a22f787b4e122ae3510e921719abe437
Sharif <fathifarhan21@hotmail.com>
2022-04-01 21:37:11 +0100

    --

--- musicPlayerApi/urls.py
+++ musicPlayerApi/urls.py
@@ -7,4 +7,5 @@ from . import views
 urlpatterns = [
     path('', views.Api, name='Api'),
     path('Api/MusicList', views.MusicList, name='MusicList'),
+    path('Api/SongSelected/<int:pk>', views.SongSelected, name='SongSelected'),
 ]
--- musicPlayerApi/views.py
+++ musicPlayerApi/views.py
@@ -15,8 +15,25 @@ def Api(request):
 @api_view(['GET'])
 def MusicList(request):
 
-    if request.method == 'GET':
+    try:
         Musics = Music.objects.all()
+    except Musics.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    print(Musics)
+    if request.method == 'GET':
         serializer = MusicSerializer(Musics, many=True)
         return Response(serializer.data)
 
+
+@api_view(['GET'])
+def SongSelected(request, pk):
+
+    try:
+        Musics = Music.objects.filter(id=pk)
+    except Musics.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = MusicSerializer(Musics, many=True)
+        return Response(serializer.data)
